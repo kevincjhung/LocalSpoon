5d78dbfadc63b5b325a4c71cb1d90e09bc5e16c0
Kevin Hung <kevincjhung@gmail.com>
2023-12-21 10:51:16 -0800

    feature: basic navigation and page routing
    - Created a basic navigation bar for testing purposes. It will be replaced with an actual one later on.
    - Implemented basic react routing for seller pages

--- localSpoon/src/App.tsx
+++ localSpoon/src/App.tsx
@@ -1,13 +1,42 @@
 import * as React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
+const Home = () => <div><p>Home</p></div>
+const CreateStore = () => <div><p>Create a Store</p></div>;
+const SellerLanding = () => <div><p>Seller Landing</p></div>;
+const Analytics = () => <div><p>Analytics</p></div>;
+const ProductPosting = () => <div><p>Product Posting</p></div>;
+const SellerOrders = () => <div><p>Orders from Customers for a Given Seller</p></div>;
+const ShopLanding = () => <div><p>Shop Landing Page</p></div>;
+
+
 function App() {
-  
 
   return (
-    <div>
-      <p>App.tsx</p>
-    </div>
+    <React.StrictMode>
+      <BrowserRouter>
+      <nav className="navbar">
+          <ul className="nav-list">
+            <li className="nav-item"><Link to="/create-store" className="nav-link">Create a Store</Link></li>
+            <li className="nav-item"><Link to="/seller" className="nav-link">Seller Landing</Link></li>
+            <li className="nav-item"><Link to="/seller/analytics" className="nav-link">Analytics</Link></li>
+            <li className="nav-item"><Link to="/seller/post-product" className="nav-link">Product Posting</Link></li>
+            <li className="nav-item"><Link to="/seller/orders" className="nav-link">Orders</Link></li>
+            <li className="nav-item"><Link to="/seller/shop" className="nav-link">Shop</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-store" element={<CreateStore />} />
+          <Route path="/seller" element={<SellerLanding />} />
+          <Route path="/seller/analytics" element={<Analytics />} />
+          <Route path="/seller/post-product" element={<ProductPosting />} />
+          <Route path="/seller/orders" element={<SellerOrders />} />
+          <Route path="/seller/shop" element={<ShopLanding />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
   )
 }
 
--- localSpoon/src/main.tsx
+++ localSpoon/src/main.tsx
@@ -1,22 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-const Home = () => <div><p>Home</p></div>
-const About = () => <div><p>About</p></div>
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"  element={<Home />} />
-        <Route path="/About"  element={<About />} />
-      </Routes>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!)
+  .render(
+  <App />
 )
