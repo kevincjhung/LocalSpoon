f7c997b956a971aa452c243b2fd8681a7f63cbfa
Kevin Hung <kevincjhung@gmail.com>
2023-12-24 11:05:41 -0800

    Update App.tsx

--- localSpoon/src/App.tsx
+++ localSpoon/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <h1>Count: {count}</h1>
+      <h1>Count = {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   )
