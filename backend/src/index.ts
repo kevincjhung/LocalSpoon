73c764b27951e3aeebe311f1553197faf87657e3
Kevin Hung <kevincjhung@gmail.com>
2024-01-02 18:05:23 -0800

    feature: init express server with basic libraries & prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,153 +1,53 @@
-import { Prisma, PrismaClient } from '@prisma/client'
 import express from 'express'
 
-
+import { Prisma, PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
+import createError from 'http-errors';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+
 const app = express()
 
 app.use(express.json())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.post(`/signup`, async (req, res) => {
-  const { name, email, posts } = req.body
 
-  const postData = posts?.map((post: Prisma.PostCreateInput) => {
-    return { title: post?.title, content: post?.content }
-  })
-
-  const result = await prisma.user.create({
-    data: {
-      name,
-      email,
-      posts: {
-        create: postData,
-      },
-    },
-  })
-  res.json(result)
+app.get('/', async (req, res) => {
+  res.json({ hello: 'world' })
 })
 
-app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      author: { connect: { email: authorEmail } },
-    },
-  })
-  res.json(result)
+app.post('/', async (req, res) => {
+  const { name, email } = req.body
+  res.json({ name, email })
 })
 
-app.put('/post/:id/views', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const post = await prisma.post.update({
-      where: { id: Number(id) },
-      data: {
-        viewCount: {
-          increment: 1,
-        },
-      },
-    })
-
-    res.json(post)
-  } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
-  }
-})
-
-app.put('/publish/:id', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const postData = await prisma.post.findUnique({
-      where: { id: Number(id) },
-      select: {
-        published: true,
-      },
-    })
-
-    const updatedPost = await prisma.post.update({
-      where: { id: Number(id) || undefined },
-      data: { published: !postData?.published },
-    })
-    res.json(updatedPost)
-  } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
-  }
-})
-
-app.delete(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-  const post = await prisma.post.delete({
-    where: {
-      id: Number(id),
-    },
-  })
-  res.json(post)
-})
 
 app.get('/users', async (req, res) => {
   const users = await prisma.user.findMany()
   res.json(users)
 })
 
-app.get('/user/:id/drafts', async (req, res) => {
-  const { id } = req.params
 
-  const drafts = await prisma.user
-    .findUnique({
-      where: {
-        id: Number(id),
-      },
-    })
-    .posts({
-      where: { published: false },
-    })
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-  res.json(drafts)
-})
+app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.get(`/post/:id`, async (req, res) => {
-  const { id }: { id?: string } = req.params
+  // render the error page
+  res.status(err.status || 500).json({ error: err.message });
+});
 
-  const post = await prisma.post.findUnique({
-    where: { id: Number(id) },
-  })
-  res.json(post)
-})
-
-app.get('/feed', async (req, res) => {
-  const { searchString, skip, take, orderBy } = req.query
-
-  const or: Prisma.PostWhereInput = searchString
-    ? {
-        OR: [
-          { title: { contains: searchString as string } },
-          { content: { contains: searchString as string } },
-        ],
-      }
-    : {}
-
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-      ...or,
-    },
-    include: { author: true },
-    take: Number(take) || undefined,
-    skip: Number(skip) || undefined,
-    orderBy: {
-      updatedAt: orderBy as Prisma.SortOrder,
-    },
-  })
-
-  res.json(posts)
-})
 
 const server = app.listen(3000, () =>
   console.log(`
-🚀 Server ready at: http://localhost:3000
-⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
+🚀 Server ready at: http://localhost:3000/users`),
 )
