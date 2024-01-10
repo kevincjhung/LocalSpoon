3e293eea530f405aa7439a47a82a7ca0dd04b7ba
Kevin Hung <kevincjhung@gmail.com>
2024-01-09 17:11:57 -0800

    feature: Analytics page grid layout

--- localSpoon/src/pages/seller/AnalyticsPage.tsx
+++ localSpoon/src/pages/seller/AnalyticsPage.tsx
@@ -1,10 +1,12 @@
-import TopSellingProducts from "../../components/analytics/TopSellingProducts"
+// import TopSellingProducts from "../../components/analytics/TopSellingProducts"
 
 export default function AnalyticsPage(){
   return (
-    <div>
-      <div className="p-4 ">
-        <TopSellingProducts/>
+    <div className="analytics-page-grid-container">
+      <div className="analytics-page-left-column">Left Column</div>
+      <div className="analytics-page-right-column">
+        <div className="analytics-page-top-card">Top Card</div>
+        <div className="analytics-page-bottom-card">Bottom Card</div>
       </div>
     </div>
   )
