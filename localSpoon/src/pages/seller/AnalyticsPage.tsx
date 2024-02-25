1ad292791e575e4a079c4766214c5787c0c4413c
Kevin Hung <kevincjhung@gmail.com>
2024-02-24 18:10:07 -0800

    Analytics page styling, removed secondary navbar

--- localSpoon/src/pages/seller/AnalyticsPage.tsx
+++ localSpoon/src/pages/seller/AnalyticsPage.tsx
@@ -17,39 +17,7 @@ export default function AnalyticsPage() {
 
   return (
     <div className="analytics-page-content">
-      <div className="analytics-page-secondary-navbar ">
-        <div className='mb-1'>
-          <Box>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              indicatorColor="secondary"
-              aria-label="secondary tabs example"
-            >
-              <Tab
-                value="Analytics Dashboard"
-                label={<Link to="/seller/analytics/dashboard">Analytics Dashboard</Link>}
-              />
-              <Tab
-                value="Sales Analytics"
-                label={<Link to="/seller/analytics/sales-analytics">Sales Analytics</Link>}
-              />
-              <Tab
-                value="Purchase Order Analytics"
-                label={<Link to="/seller/analytics/purchase-order-analytics">Purchase Order Analytics</Link>}
-              />
-              <Tab
-                value="Top Sellers Analytics"
-                label={<Link to="/seller/analytics/top-sellers">Top Sellers</Link>}
-              />
-              <Tab
-                value="Revenue Analytics"
-                label={<Link to="/seller/analytics/revenue-analytics">Revenue Analytics</Link>}
-              />
-            </Tabs>
-          </Box>
-        </div>
-      </div>
+      
       <div className="analytics-page-grid-container m-6">
         <div className="analytics-page-top-row">
           <PastSalesByStoreLinechartCard storeId={1}/>
